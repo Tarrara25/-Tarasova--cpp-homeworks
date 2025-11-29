ebde18c34cf7a1c3ce8a76719704ed56b4e0ff30
Tarrara25 <tarasovaanna251205@mail.ru>
2025-11-29 17:15:37 +0500

    hw12

---
+++ homework12.1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+// объявляем класс Rectangle и создаем приватные поля(закрытые)
+class Rectangle {
+private:
+    double width;
+    double height;
+
+
+public:
+    // конструктор по умолчанию для создания прямоугольника с нулевыми размерами
+    Rectangle() {
+        width = 0.0;
+        height = 0.0;
+    }
+
+    // конструктор с параметрами для создания прямоугольника с заданными размерами
+    Rectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    // (геттеры) узнаем значения приватных полей
+    double getWidth() {
+        return width;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    // (сеттеры) меняем значения приватных полей
+    void setWidth(double w) {
+        width = w;
+    }
+
+    void setHeight(double h) {
+        height = h;
+    }
+
+    // показываем ширину и высоту
+    void show() {
+        // fixed и setprecision для вывода с десятичной точкой
+        cout << fixed << setprecision(1); // 1 цифра после точки
+        cout << "Ширина: " << width << endl;
+        cout << "Высота: " << height << endl;
+    }
+};
+
+int main() {
+    Rectangle rect(4.0, 3.0);
+    rect.show();
+    return 0;
+}
